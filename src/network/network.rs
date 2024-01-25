eac4202d65d29be6680533bb5343f3d34f4254d4
Braden Everson <bradeneverson@gmail.com>
2024-01-24 19:52:37 -0600

    Added .triton parsing and massively downsized unneeded methods in network

--- src/network/network.rs
+++ src/network/network.rs
@@ -1,50 +1,24 @@
-use super::layer::distributions::Distributions;
-use super::layer::layers::{Layer, LayerTypes};
-use super::layer::dense::Dense;
-use super::layer::noise::gen_noise;
-use super::layer::pair::GradientPair;
-use super::matrix::Matrix;
+use super::layer::layers::Layer;
 use super::input::Input;
-use rand::{RngCore, Rng, thread_rng};
-use rand_pcg::Pcg64;
-use rand_seeder::Seeder;
+use super::serialize::ser_layer::SerializedLayer;
+use rand::{RngCore, thread_rng};
 use serde::{Serialize, Deserialize};
-
-use futures::stream::{StreamExt, FuturesUnordered};
-
-use serde_json::{to_string, from_str};
-use std::io;
-use std::ops::Range;
-use std::{
-    fs::File,
-    io::{Read,Write},
-};
 
 #[derive(Serialize, Deserialize)]
 pub struct Network {
-    batch_size: usize,
-    pub layer_sizes: Vec<usize>,
-    pub loss: f32,
-    loss_train: Vec<f32>,
     pub layers: Vec<Box<dyn Layer>>,
-    uncompiled_layers: Vec<LayerTypes>,
-    seed: Option<String>,
-    #[serde(skip)]
-    #[serde(default = "Network::thread_rng")]
-    rng: Box<dyn RngCore>
 }
 
 impl Network{
+    pub fn new() -> Network {
+        Network { layers: vec![] }
+    }
     fn thread_rng() -> Box<dyn RngCore> {
         Box::new(thread_rng())
     }
     pub fn predict(&mut self, input: &dyn Input) -> Vec<f32>{
         let in_box: Box<dyn Input> = input.to_box();
         self.feed_forward(&in_box)
-    }
-    pub fn set_seed(&mut self, seed: &str){
-        self.seed = Some(String::from(seed));
-        self.rng = self.get_rng();
     }
     ///Travels through a neural network's abstracted Layers and returns the resultant vector at the
     ///end
@@ -80,37 +54,13 @@ impl Network{
         }
         data_at.to_param().to_owned()
     }
-    fn get_rng(&self) -> Box<dyn RngCore> {
-        match &self.seed {
-            Some(seed_rng) => Box::new(Seeder::from(seed_rng).make_rng::<Pcg64>()),
-            None => Box::new(rand::thread_rng())
+    pub fn deserialize_triton_fmt_string(format_string: String) -> Network {
+        let mut net: Network = Network::new();
+        let parse_triton = format_string.split("#");
+        for layer in parse_triton {
+            let new_layer: Box<dyn Layer> = SerializedLayer::from_string(layer.to_string()).from();
+            net.layers.push(new_layer);
         }
-    }
-    pub fn load(path: &str) -> Network{
-        let mut buffer = String::new();
-        let mut file = File::open(path).expect("Unable to read file :(");
-
-        file.read_to_string(&mut buffer).expect("Unable to read file but even sadder :(");
-
-        let mut net: Network = from_str(&buffer).expect("Json was not formatted well >:(");
-        net.rng = net.get_rng();
         net
-    }
-    pub fn load_str(json_string: &str) -> Network {
-        let mut net: Network = from_str(json_string).expect("Improper json format");
-        net.rng = net.get_rng();
-        net
-    }
-    pub fn load_cbor(path: &str) -> Result<Network, serde_cbor::Error> {
-        let file = File::open(path).expect("error loading file");
-        let mut network: Network = serde_cbor::from_reader(file)?;
-        network.rng = network.get_rng();
-        Ok(network)
-    }
-    pub fn to_vec(&self) -> Result<Vec<u8>, serde_cbor::Error> {
-        serde_cbor::to_vec(self)
-    }
-    pub fn from_vec(data: Vec<u8>) -> Result<Network, serde_cbor::Error> {
-        serde_cbor::from_slice(&data[..])
     }
 }
